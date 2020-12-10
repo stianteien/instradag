dc279460c47d23a6834068c55e6e832377b07848
Stian Teien <stian.teien@gmail.com>
2020-12-10 22:34:43 +0100

    glemte å overskrive stock

--- lib/make_ready.py
+++ lib/make_ready.py
@@ -38,8 +38,8 @@ class make_ready:
             stock['derivert'] = [stock.open[i] / stock.open[i-1] if i>1 else 1
                                  for i, value in enumerate(stock.open)]
     
-            # Fill nan verdier med 0
-            stock.fillna(.5)
+            # Fill nan verdier med .5
+            stock = stock.fillna(.5)
             stocks.append(stock)
         
         return stocks
