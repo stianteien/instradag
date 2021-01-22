3b08ce41da77d5188d7c6e05616d2ec2865c734b
Stian Teien <stian.teien@gmail.com>
2021-01-22 22:32:41 +0100

    pipeline to dataclean

---
+++ lib/to_data_clean.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 15 15:17:58 2021
+
+@author: Stian
+"""
+
+import os
+import pandas as pd
+import numpy as np
+from make_ready import make_ready
+
+filer = os.listdir('../data')
+
+for fil in filer:
+    df = make_ready().use_stockstats(['../data/'+fil])[0]
+    df.to_excel('../data_clean/'+fil, index=False)
